ce54af910d114770783b26c96be2c5ace8e431c2
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-03 17:07:53 -0300

    chore(terraform): update dbt-core-service-account roles

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -3,16 +3,16 @@ resource "google_service_account" "dbt_core_service_account" {
   display_name = "Mathews Recurring Revenue dbt_core Service Account"
 }
 
-resource "google_project_iam_member" "dbt_core_service_account_bigquery_job_user_role" {
+resource "google_project_iam_member" "dbt_core_service_account_bigquery_user_role" {
   project = var.mathews_recurring_revenue_project_id
-  role    = "roles/bigquery.jobUser"
+  role    = "roles/bigquery.user"
   member  = "serviceAccount:${google_service_account.dbt_core_service_account.email}"
 }
 
-resource "google_bigquery_dataset_access" "dbt_core_service_account_bigquery_dataset_reader_role" {
-  dataset_id    = google_bigquery_dataset.google_sheets_raw_dataset.dataset_id
-  role          = "READER"
-  user_by_email = google_service_account.dbt_core_service_account.email
+resource "google_project_iam_member" "dbt_core_service_account_bigquery_data_editor_role" {
+  project = var.mathews_recurring_revenue_project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:${google_service_account.dbt_core_service_account.email}"
 }
 
 output "dbt_core_service_account_email" {
