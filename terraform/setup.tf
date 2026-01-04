00e929bd8c1ef035d4ad64ef7badf7f3f0163fcc
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-04 01:19:48 -0300

    chore(terraform): shrink variables

--- terraform/setup.tf
+++ terraform/setup.tf
@@ -1,12 +1,16 @@
+# gcloud auth application-default login \
+#  --impersonate-service-account terraform@mathews-recurring-revenue.iam.gserviceaccount.com \
+#  --scopes="https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/drive"
+
 # gcloud iam service-accounts add-iam-policy-binding \
-#   terraform-service-account@mathews-recurring-revenue.iam.gserviceaccount.com \
+#   terraform@mathews-recurring-revenue.iam.gserviceaccount.com \
 #   --project="mathews-recurring-revenue" \
 #   --member="user:mathews.machadoamorim@gmail.com" \
 #   --role="roles/iam.serviceAccountTokenCreator"
 
 provider "google" {
-  project = var.mathews_recurring_revenue_project_id
-  region  = var.mathews_recurring_revenue_project_region
+  project = var.project_id
+  region  = var.project_region
 }
 
 terraform {
@@ -20,17 +24,7 @@ terraform {
   required_version = "~> 1.12.2"
 
   backend "gcs" {
-    impersonate_service_account = "terraform-service-account@mathews-recurring-revenue.iam.gserviceaccount.com"
+    impersonate_service_account = "terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
     bucket                      = "mathews-recurring-revenue-terraform-bucket"
   }
-}
-
-import {
-  to = google_service_account.terraform_service_account
-  id = "projects/mathews-recurring-revenue/serviceAccounts/terraform-service-account@mathews-recurring-revenue.iam.gserviceaccount.com"
-}
-
-import {
-  to = google_storage_bucket.mathews_recurring_revenue_terraform_bucket
-  id = "mathews-recurring-revenue-terraform-bucket"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,17 @@
-variable "mathews_recurring_revenue_project_id" {
-  description = "The ID of the Google Cloud project"
+variable "project_id" {
+  description = "Google Cloud Project Id"
   type        = string
   default     = "mathews-recurring-revenue"
 }
 
-variable "mathews_recurring_revenue_project_region" {
-  description = "The Google Cloud region"
+variable "project_location" {
+  description = "Google Cloud Region"
+  type        = string
+  default     = "US"
+}
+
+variable "project_region" {
+  description = "Google Cloud Region"
   type        = string
   default     = "us-central1"
 }
