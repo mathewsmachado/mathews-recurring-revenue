ab83cc01ddf30837bbee70eca04460590e36a283
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-14 21:59:01 -0300

    chore(terraform): simplify terraform service account roles assignment

--- terraform/setup.tf
+++ terraform/setup.tf
@@ -8,75 +8,57 @@
 #   --member="user:mathews.machadoamorim@gmail.com" \
 #   --role="roles/iam.serviceAccountTokenCreator"
 
+# Provider
 provider "google" {
   project = var.project_id
   region  = var.project_region
 }
 
+# Terraform Service Account
 resource "google_service_account" "terraform" {
-  account_id                   = "terraform"
-  create_ignore_already_exists = null
-  description                  = "Terraform Service Account"
-  disabled                     = false
-  display_name                 = "terraform"
   project                      = "mathews-recurring-revenue"
+  description                  = "Terraform Service Account"
+  account_id                   = "terraform"
+  display_name                 = "terraform"
+  create_ignore_already_exists = null
+  disabled                     = false
 }
 
-resource "google_project_iam_member" "iam_service_account_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/iam.serviceAccountAdmin"
+# Terraform Service Account Roles
+locals {
+  roles = [
+    "bigquery.admin",
+    "iam.serviceAccountAdmin",
+    "iam.serviceAccountTokenCreator",
+    "resourcemanager.projectIamAdmin",
+    "storage.admin",
+    "storage.objectAdmin",
+    "serviceusage.serviceUsageAdmin"
+  ]
 }
 
-resource "google_project_iam_member" "resourcemanager_project_iam_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/resourcemanager.projectIamAdmin"
+resource "google_project_iam_member" "terraform_service_account_roles" {
+  for_each = toset(local.roles)
+
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.terraform.email}"
+  role    = "roles/${each.value}"
 }
 
-resource "google_project_iam_member" "storage_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/storage.admin"
-}
-
-resource "google_project_iam_member" "bigquery_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/bigquery.admin"
-}
-
-resource "google_project_iam_member" "iam_service_account_token_creator" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/iam.serviceAccountTokenCreator"
-}
-
-resource "google_project_iam_member" "storage_object_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/storage.objectAdmin"
-}
-
-resource "google_project_iam_member" "serviceusage_service_usage_admin" {
-  member  = "serviceAccount:terraform@mathews-recurring-revenue.iam.gserviceaccount.com"
-  project = "mathews-recurring-revenue"
-  role    = "roles/serviceusage.serviceUsageAdmin"
-}
-
+# Terraform State Bucket
 resource "google_storage_bucket" "mathews_recurring_revenue_terraform_bucket" {
-  default_event_based_hold    = false
-  enable_object_retention     = false
-  force_destroy               = false
-  labels                      = {}
+  project                     = "mathews-recurring-revenue"
   location                    = "US"
   name                        = "mathews-recurring-revenue-terraform-bucket"
-  project                     = "mathews-recurring-revenue"
-  public_access_prevention    = "enforced"
-  requester_pays              = false
-  rpo                         = "DEFAULT"
   storage_class               = "STANDARD"
+  public_access_prevention    = "enforced"
+  rpo                         = "DEFAULT"
+  force_destroy               = false
+  default_event_based_hold    = false
+  enable_object_retention     = false
+  requester_pays              = false
   uniform_bucket_level_access = true
+  labels                      = {}
 
   hierarchical_namespace {
     enabled = false
@@ -87,6 +69,7 @@ resource "google_storage_bucket" "mathews_recurring_revenue_terraform_bucket" {
   }
 }
 
+# Terraform Block
 terraform {
   required_providers {
     google = {
