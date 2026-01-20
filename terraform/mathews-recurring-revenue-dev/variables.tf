5a14d182efb3de1ea51f06857e503f10eb4dca37
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-20 20:49:45 -0300

    chore(terraform): add dev project variables

---
+++ terraform/mathews-recurring-revenue-dev/variables.tf
@@ -0,0 +1,23 @@
+variable "project_id" {
+  description = "Google Cloud Project Id"
+  #   type        = string
+  default = "mathews-recurring-revenue-dev"
+}
+
+variable "project_location" {
+  description = "Google Cloud Region"
+  #   type        = string
+  default = "US"
+}
+
+variable "project_region" {
+  description = "Google Cloud Region"
+  #   type        = string
+  default = "us-central1"
+}
+
+variable "env" {
+  description = "Environment"
+  #   type        = string
+  default = "dev"
+}
