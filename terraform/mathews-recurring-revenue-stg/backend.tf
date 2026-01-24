4c76efbc242745b63c5ec1d71f982ea8a734773d
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-24 00:12:05 -0300

    chore(terraform): create stg project necessary assets (buckets, service account, permissions)

--- terraform/mathews-recurring-revenue-stg/backend.tf
+++ terraform/mathews-recurring-revenue-stg/backend.tf
@@ -3,11 +3,11 @@ Terraform needs the following steps to work:
 
     1. Create a GCP project
 
-    2. Create a Service Account for Terraform:
+    2. Create a Service Account for GitHub per environment:
         ```sh
-        gcloud iam service-accounts create terraform \
-            --project="mathews-recurring-revenue-dev" \
-            --display-name="Terraform Service Account"
+        gcloud iam service-accounts create github-actions \
+            --project="mathews-recurring-revenue-stg" \
+            --display-name="GitHub Actions Service Account"
         ```
 
     3. Assign the following roles to Terraform's Service Account:
@@ -21,32 +21,25 @@ Terraform needs the following steps to work:
             roles/storage.admin \
             roles/serviceusage.serviceUsageAdmin; \
             do \
-                gcloud projects add-iam-policy-binding mathews-recurring-revenue-dev \
-                    --member="serviceAccount:terraform@mathews-recurring-revenue-dev.iam.gserviceaccount.com" \
+                gcloud projects add-iam-policy-binding mathews-recurring-revenue-stg \
+                    --member="serviceAccount:github-actions@mathews-recurring-revenue-stg.iam.gserviceaccount.com" \
                     --role="$role"; \
             done
         ```
 
-    4. Enable Credentials API:
-        ```sh
-        gcloud services enable iamcredentials.googleapis.com \
-            --project mathews-recurring-revenue-dev
-        ```
+    # 4. Enable Credentials API:
+    #     ```sh
+    #     gcloud services enable iamcredentials.googleapis.com \
+    #         --project mathews-recurring-revenue-stg
+    #     ```
 
-    5. Authorize your personal user to impersonate Terraform's Service Account:
+    5. Allow the Workload Identity Pool to impersonate the Service Account
         ```sh
-        gcloud iam service-accounts add-iam-policy-binding terraform@mathews-recurring-revenue-dev.iam.gserviceaccount.com \
-            --project="mathews-recurring-revenue-dev" \
-            --member="user:mathews.machadoamorim@gmail.com" \
-            --role="roles/iam.serviceAccountTokenCreator"
-        ```
-
-    6. Reauthenticate, impersonating Terraform's Service Account:
-        ```sh
-        gcloud auth application-default login \
-            --project="mathews-recurring-revenue-dev" \
-            --impersonate-service-account="terraform@mathews-recurring-revenue-dev.iam.gserviceaccount.com" \
-            --scopes="https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/drive"
+        gcloud iam service-accounts add-iam-policy-binding \
+            "github-actions@mathews-recurring-revenue-stg.iam.gserviceaccount.com" \
+            --project="mathews-recurring-revenue-stg" \
+            --role="roles/iam.workloadIdentityUser" \
+            --member="principalSet://iam.googleapis.com/projects/724228842720/locations/global/workloadIdentityPools/github/attribute.repository/mathewsmachado/mathews-recurring-revenue"
         ```
 
     7. Create a bucket to store Terraform's remote state
@@ -71,14 +64,14 @@ terraform {
   required_version = "~> 1.12.2"
 
   backend "gcs" {
-    # impersonate_service_account = "terraform@mathews-recurring-revenue-dev.iam.gserviceaccount.com"
+    # impersonate_service_account = "github-actions@mathews-recurring-revenue-stg.iam.gserviceaccount.com"
     bucket                      = "mathews-recurring-revenue-stg-terraform-bucket"
     prefix                      = "state"
   }
 }
 
 provider "google" {
+  # impersonate_service_account = "github-actions@mathews-recurring-revenue-stg.iam.gserviceaccount.com"
   project                     = var.project_id
   region                      = var.project_region
-  # impersonate_service_account = "terraform@mathews-recurring-revenue-dev.iam.gserviceaccount.com"
 }
