29e5390371e1e6cc2e6861e690f88ab0c04f85b5
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-23 23:44:34 -0300

    chore(terraform): add stg project variables

---
+++ terraform/mathews-recurring-revenue-stg/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  description = "Google Cloud Project Id"
+  default = "mathews-recurring-revenue-stg"
+}
+
+variable "project_location" {
+  description = "Google Cloud Location"
+  default = "US"
+}
+
+variable "project_region" {
+  description = "Google Cloud Region"
+  default = "us-central1"
+}
+
+variable "env" {
+  description = "Environment"
+  default = "stg"
+}
