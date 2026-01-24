a03e99aa940081790e40501856a53b7c91ec6280
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-23 23:43:22 -0300

    chore(terraform): fix project_location variable description on dev project

--- terraform/mathews-recurring-revenue-dev/variables.tf
+++ terraform/mathews-recurring-revenue-dev/variables.tf
@@ -1,23 +1,19 @@
 variable "project_id" {
   description = "Google Cloud Project Id"
-  #   type        = string
   default = "mathews-recurring-revenue-dev"
 }
 
 variable "project_location" {
-  description = "Google Cloud Region"
-  #   type        = string
+  description = "Google Cloud Location"
   default = "US"
 }
 
 variable "project_region" {
   description = "Google Cloud Region"
-  #   type        = string
   default = "us-central1"
 }
 
 variable "env" {
   description = "Environment"
-  #   type        = string
   default = "dev"
 }
