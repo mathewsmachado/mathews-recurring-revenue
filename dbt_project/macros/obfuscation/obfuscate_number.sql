1e90acf5ab99eb3b9931bac5d363ff556dec43b0
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-28 21:18:21 -0300

    feat(dbt_project): improve obfuscate_number macro

--- dbt_project/macros/obfuscation/obfuscate_number.sql
+++ dbt_project/macros/obfuscation/obfuscate_number.sql
@@ -1,18 +1,28 @@
 {%-
     macro obfuscate_number(
         column,
+        pepper,
         slope,
         jitter,
-        salt,
-        lower_bound,
-        upper_bound
+        lower_bound=0,
+        upper_bound=1000
     )
 -%}
-    (
-        (
-            cume_dist() over (order by {{ column }})
-            * ({{ lower_bound }} + mod(abs(farm_fingerprint('{{ salt }}')), {{ upper_bound }}))
-            * {{ slope }}
-        ) + {{ jitter }}
-    )
+    {%- if not column -%}
+        {{ exceptions.raise_compiler_error("Parameter 'column' is required.") }}
+    {%- endif -%}
+
+    {%- if not pepper -%}
+        {{ exceptions.raise_compiler_error("Parameter 'pepper' is required.") }}
+    {%- endif -%}
+
+    {%- if not slope -%}
+        {{ exceptions.raise_compiler_error("Parameter 'slope' is required.") }}
+    {%- endif -%}
+
+    {%- if not jitter -%}
+        {{ exceptions.raise_compiler_error("Parameter 'jitter' is required.") }}
+    {%- endif -%}
+
+    ((cume_dist() over (order by {{ column }}) * ({{ lower_bound }} + mod(abs(farm_fingerprint('{{ pepper }}')), {{ upper_bound }})) * {{ slope }}) + {{ jitter }})
 {%- endmacro -%}
