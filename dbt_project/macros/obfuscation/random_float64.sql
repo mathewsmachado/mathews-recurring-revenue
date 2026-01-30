d92479ece3c38b98158c581bb16c9402f7fce10a
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-01-29 23:53:37 -0300

    feat(dbt_project): add random_float64 macro

---
+++ dbt_project/macros/obfuscation/random_float64.sql
@@ -0,0 +1,3 @@
+{%- macro random_float64(lower_bound=0, upper_bound=1000) -%}
+    {{ lower_bound }} + (abs(farm_fingerprint(generate_uuid())) / pow(2, 63)) * ({{ upper_bound }} - {{ lower_bound }})
+{%- endmacro -%}
