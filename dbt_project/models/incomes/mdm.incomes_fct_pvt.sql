b9d07d80df849bbaf52be8487e3eb8ffa57e8dbc
Mathews Machado <mathews.machadoamorim@gmail.com>
2026-02-28 19:11:28 -0300

    ci(dbt_project): add change to trigger dbt deploy

--- dbt_project/models/incomes/mdm.incomes_fct_pvt.sql
+++ dbt_project/models/incomes/mdm.incomes_fct_pvt.sql
@@ -7,7 +7,7 @@ with source as (
 prepared as (
     select
         * except(tran_gross),
-        coalesce(tran_gross, tran_net) as tran_gross
+        coalesce(tran_gross, tran_net) as tran_gross,
     from source
 ),
 enriched as (
@@ -16,7 +16,7 @@ enriched as (
         to_hex(md5(concat(tran_category, tran_payee, tran_date))) as tran_id,
         tran_gross - tran_net as tran_taxes,
         (tran_gross - tran_net) / tran_gross * 100 as tran_taxes_pct,
-        timestamp("{{ _processed_at }}") as _processed_at
+        timestamp("{{ _processed_at }}") as _processed_at,
     from prepared
 ),
 final as (
@@ -38,7 +38,7 @@ final as (
         accounting_year,
         accounting_month,
         accounting_status,
-        _processed_at
+        _processed_at,
     from enriched
 )
 select *
